c0c2451fea82e070feed9d7016d37454d546a9b5
bonno123 <banikavik3@gmail.com>
2023-12-25 02:46:42 +0530

    feat: integrate with  astro-seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bonno123.github.io',
-    base: '/profile',
+    site: 'https://avikbanik.com'
 });
