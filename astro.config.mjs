9f4dfda68deb5d5649041a13bd4a501b8d9deae6
bonno123 <banikavik3@gmail.com>
2024-01-05 04:45:38 +0530

    feat: add tresjs <cientos composables library dos not work>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://avikbanik.com'
+  site: 'https://avikbanik.com',
+  integrations: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => (tag.startsWith('Tres') || tag === 'primitive' ) && tag !== 'TresCanvas',
+        },
+      },
+    }),
+  ],
 });
