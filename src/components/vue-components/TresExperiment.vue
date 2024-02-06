08caf4ea7af98a2eb4bb4e3c7bf69e1cb53d070d
bonno123 <banikavik3@gmail.com>
2024-02-07 04:33:04 +0530

    feat: add 3D earth mesh

--- src/components/vue-components/TresExperiment.vue
+++ src/components/vue-components/TresExperiment.vue
@@ -1,45 +1,93 @@
 <template>
     <div class="tres-container">
         <TresCanvas v-bind="gl">
-            <TresPerspectiveCamera :args="[45, 1, 0.1, 1000]" />
-            <!-- <primitive :object="meshWithMaterial" /> -->
+            <TresPerspectiveCamera :args="[45, 1, 0.1, 1000]"  />
 
             <TresMesh
                 ref="boxRef"
                 :scale="1"
             >
-                <TresBoxGeometry :args="[1.5, 1.5, 1.5]" />
-                <TresMeshNormalMaterial />
+                <!-- <TresBoxGeometry :args="[1.5, 1.5, 1.5]" /> -->
+                <TresIcosahedronGeometry :args="[1, 4]" />
+
+                <TresMeshStandardMaterial
+                    v-bind="pbrTexture"
+                    :displacement-scale="0.1"
+                />
+
+                <OrbitControls shadows alpha />
+
             </TresMesh>
+
+            <TresPointLight>
+                <Lensflare 
+                    :elements="[{ color: 'red' }, { color: 'yellow' }]" 
+                    :seed="5" 
+                />
+            </TresPointLight>
+            <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
+            <TresAmbientLight :color="(Color.NAMES['blueviolet'] as unknown as Color)" /> 
+            <TresDirectionalLight :position="[0, 2, 4]" :intensity="2" cast-shadow />
+
+
+            <Stars></Stars>
+
+            <!-- <TresGridHelper /> -->
         </TresCanvas>
     </div>
 </template>
 
 <script setup lang="ts">
-import { TresCanvas, useRenderLoop } from '@tresjs/core';
+import { TresCanvas, useLoader, useRenderLoop, useTexture  } from '@tresjs/core';
 import type { TresCanvasProps } from '@tresjs/core/dist/components/TresCanvas.vue.js';
-import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three';
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Color } from 'three';
 import type { RendererElement } from 'vue';
 import { shallowRef, type ShallowRef } from 'vue';
+import { OrbitControls, Lensflare, Stars } from '@tresjs/cientos';
 
 const { onLoop } = useRenderLoop()
 
+// const pbrTexture = await useTexture({
+//   map: 'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Displacement.jpg',
+//   displacementMap:
+//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Displacement.jpg',
+//   roughnessMap:
+//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Roughness.jpg',
+//   normalMap:
+//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_NormalGL.jpg',
+//   ambientOcclusion:
+//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_AmbientOcclusion.jpg',
+// });
+
+const pbrTexture = await useTexture({
+  map: '/earthbump1k.jpg',
+  displacementMap:
+    'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Displacement.jpg',
+  roughnessMap:
+    'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Roughness.jpg',
+  normalMap:
+    'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_NormalGL.jpg',
+  ambientOcclusion:
+    'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_AmbientOcclusion.jpg',
+});
+
 const boxRef: ShallowRef<RendererElement | null> = shallowRef(null)
+// const pointLightRef: ShallowRef<RendererElement | null> = shallowRef(null)
 
 const gl = {
-    clearColor: '#FFF',
+    clearColor: '#000',
     shadows: true,
     alpha: false,
     shadowMapType: BasicShadowMap,
     outputColorSpace: SRGBColorSpace,
     toneMapping: NoToneMapping,
-    // antialias: true,
-    // depth: true,
-    // stencil: true,
-    // premultipliedAlpha: true,
-    // preserveDrawingBuffer: false,
-    // powerPreference: 'default',
-    // failIfMajorPerformanceCaveat: false,
+    antialias: true,
+    depth: true,
+    stencil: true,
+    premultipliedAlpha: true,
+    preserveDrawingBuffer: false,
+    powerPreference: 'default',
+    failIfMajorPerformanceCaveat: false,
     // desynchronized: false,
     // xrCompatible: false,
 } satisfies TresCanvasProps;
@@ -48,11 +96,12 @@ const gl = {
 const startAnimateBox = function () {
     onLoop(({ delta, elapsed }) => {
         if (boxRef.value) {
-            boxRef.value.rotation.y += delta
-            boxRef.value.rotation.z = elapsed * 0.2
+            boxRef.value.rotation.y += delta * 0.05
+            boxRef.value.rotation.z = elapsed * -0.05
         }
     })
 }
+
 
 startAnimateBox()
 
@@ -63,7 +112,7 @@ startAnimateBox()
 .tres-container {
     width: 100%;
     height: 350px;
-    /* aspect-ratio: 1; */
+    aspect-ratio: 1;
 }
 
 canvas {
