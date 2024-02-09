c3a07e7200630a36eeafc0ef29f1cbd01c02794f
bonno123 <banikavik3@gmail.com>
2024-02-10 04:40:17 +0530

    feat: add GLTF model exported from blender

--- src/components/vue-components/3DExperience.vue
+++ src/components/vue-components/3DExperience.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="tres-container">
-        <TresCanvas v-bind="gl">
+        <TresCanvas v-bind="gl" v-if="0">
             <TresPerspectiveCamera :args="[45, 1, 0.1, 1000]"  />
 
             <TresMesh
                 ref="boxRef"
                 :scale="1"
             >
-                <!-- <TresBoxGeometry :args="[1.5, 1.5, 1.5]" /> -->
-                <TresIcosahedronGeometry :args="[1, 4]" />
+                <TresSphereGeometry :args="[1, 32, 16]" />
 
                 <TresMeshStandardMaterial
                     v-bind="pbrTexture"
@@ -25,39 +24,51 @@
                     :seed="5" 
                 />
             </TresPointLight>
-            <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
+
             <TresAmbientLight :color="(Color.NAMES['blueviolet'] as unknown as Color)" /> 
+
             <TresDirectionalLight :position="[0, 2, 4]" :intensity="2" cast-shadow />
 
+            <Stars />
+        </TresCanvas>
 
-            <Stars></Stars>
+        <TresCanvas v-bind="gl" v-if="1">
+            <TresDirectionalLight :position="[0, 2, 4]" :intensity="4" :color="((Color.NAMES.blanchedalmond) as unknown as Color)" cast-shadow />
+            <TresDirectionalLight :position="[2, 2, 0]" :intensity="5" cast-shadow />
 
-            <!-- <TresGridHelper /> -->
+            <TresPerspectiveCamera 
+                :position="[0, 5, 5]"
+            />
+
+            <TresMesh
+                ref="boxRef"
+                :scale="1"
+            >
+                <OrbitControls shadows alpha />
+
+                <suspense>
+                    <Suzanne />
+                </suspense>
+
+            </TresMesh>
+
+            <TresPointLight :color="((Color.NAMES.sienna) as unknown as Color)" :intensity="8" :look-at="0" /> 
+
+            <Stars />
         </TresCanvas>
     </div>
 </template>
 
 <script setup lang="ts">
-import { TresCanvas, useLoader, useRenderLoop, useTexture  } from '@tresjs/core';
-import type { TresCanvasProps } from '@tresjs/core/dist/components/TresCanvas.vue.js';
+import { TresCanvas, useRenderLoop, useTexture  } from '@tresjs/core';
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping, Color } from 'three';
 import type { RendererElement } from 'vue';
 import { shallowRef, type ShallowRef } from 'vue';
 import { OrbitControls, Lensflare, Stars } from '@tresjs/cientos';
+import Suzanne from './Suzanne.vue';
+import type { TresCanvasProps } from '@tresjs/core/dist/src/components/TresCanvas.vue.js';
 
 const { onLoop } = useRenderLoop()
-
-// const pbrTexture = await useTexture({
-//   map: 'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Displacement.jpg',
-//   displacementMap:
-//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Displacement.jpg',
-//   roughnessMap:
-//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_Roughness.jpg',
-//   normalMap:
-//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_NormalGL.jpg',
-//   ambientOcclusion:
-//     'https://raw.githubusercontent.com/Tresjs/assets/main/textures/black-rock/Rock035_2K_AmbientOcclusion.jpg',
-// });
 
 const pbrTexture = await useTexture({
   map: '/earthbump1k.jpg',
@@ -72,7 +83,6 @@ const pbrTexture = await useTexture({
 });
 
 const boxRef: ShallowRef<RendererElement | null> = shallowRef(null)
-// const pointLightRef: ShallowRef<RendererElement | null> = shallowRef(null)
 
 const gl = {
     clearColor: '#000',
---
+++ src/components/vue-components/Suzanne.vue
@@ -0,0 +1,12 @@
+<template>
+    <primitive :object="model"></primitive>
+</template>
+
+<script lang="ts" setup>
+import { useGLTF } from '@tresjs/cientos';
+
+const { nodes } = await useGLTF('/monkey.glb', {draco: true});
+
+const model = nodes.Suzanne;
+
+</script>
