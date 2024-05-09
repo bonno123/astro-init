73eb73934a50034700b07cbca86836e71fb71e80
bonno123 <banikavik3@gmail.com>
2024-05-09 19:05:43 +0530

    feat: add component for scratch-reveal email

--- src/data/site-info.ts
+++ src/data/site-info.ts
@@ -46,7 +46,6 @@ export const heroWidget = {
 		<br>
 		<br>
 		Here, I have planned to share my GLSL design *playground*.
-		If you are interested, 💀 you can *subscribe* to my newsletter to get updates on what I am doing.
 
 		<br>
 		<br>
@@ -87,28 +86,23 @@ export const heroWidget = {
 	],
 }
 
+// currently this one is not using Path: src/data/about-me.ts
 export const aboutMeWidget = {
 	title: "About Me",
 	description: "portfolio of software engineer, web developer.",
 	aboutMe: {
 		title: "Hello, I’m Avik",
 		description: `
-			I am a software engineer veteran of 3+ years with the passion for web development.
-			<br>
-			<br>
-			My expertise lies in building web applications front-end part. Worked mostly 
-			with Vue.js and its ecosystem like Quasar.
-			<br>
-			I also have worked for backend a bit with Node.js and FastAPI, and databases like Oracle, postgreSQL with Prisma ORM.
-			<br>
-			<br>
-			I am currently working as a software engineer at a startup called RentApp.
-			And also a part-time freelancer and have worked with clients from the UK(Slovakia), and India.
-			I am always open to new opportunities.
-			<br>
-			<br>
-			Feel free to contact me at <strong class="fw-bold">${import.meta.env.PUBLIC_CONTACT_EMAIL}</strong>
-			or reach out to me on any of the following options!
+		I am a software engineer and web developer based in Kolkata, India.
+		<br/>
+		It's been 3 years since I started my tech journey.
+		Worked with various technologies and frameworks like Vue.js, Quasar, Node.js, FastAPI, and databases like Oracle, postgreSQL with Prisma ORM.
+		<br/><br/>
+		I am currently working as a software engineer at a startup called RentApp.
+		And also a part-time freelancer and have worked with clients from the UK(Slovakia), and India.
+		I am always open to new opportunities.
+		<br/><br/>
+		Feel free to email me or connect with me on any of the following options! 
     	`,
 		mail: import.meta.env.PUBLIC_CONTACT_EMAIL,
 	},
