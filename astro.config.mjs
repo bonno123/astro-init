514db3669f7d75cf9812556398829a7ff812317a
bonno123 <banikavik3@gmail.com>
2024-05-23 15:36:35 +0530

    exp-ssr(3): add netlify ssr adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,25 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import db from "@astrojs/db";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://avikbanik.com',
   redirects: {
     // '/this': '/'
   },
-  integrations: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: tag => (tag.startsWith('Tres') || tag === 'primitive') && tag !== 'TresCanvas'
-        }
+  integrations: [vue({
+    template: {
+      compilerOptions: {
+        isCustomElement: tag => (tag.startsWith('Tres') || tag === 'primitive') && tag !== 'TresCanvas'
       }
-    }), 
-    db()
-  ],
+    }
+  }), db()],
   build: {
-    format: 'directory',
+    format: 'directory'
   },
   trailingSlash: 'always',
   output: 'hybrid',
+  adapter: netlify()
 });
