a171a72509cb1270d057e4047bf08a7b401b6781
bonno123 <banikavik3@gmail.com>
2024-05-26 23:06:06 +0530

    fix: final, deployed URL at astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://app.avikbanik.com',
+  site: 'https://www.avikbanik.com',
   redirects: {
     '/this': '/',
   },
