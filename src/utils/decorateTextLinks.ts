5db2db3019bb6e084432bd67fe394dec6baf402c
bonno123 <banikavik3@gmail.com>
2024-05-30 19:25:05 +0530

    feat: add linked-text hover effect

--- src/utils/decorateTextLinks.ts
+++ src/utils/decorateTextLinks.ts
@@ -4,7 +4,7 @@ export function decorateTextLinks(
   return str.replace(
     /\*{1,2}(.*?)\*{1,2}/g,
     `<a
-      class="fw-bold text-accent"
+      class="fw-bold text-accent text-bg-effect text-bg-effect--scale-y"
       href="$1" 
     >
     <span class="sr-only">{$1} link</span>
